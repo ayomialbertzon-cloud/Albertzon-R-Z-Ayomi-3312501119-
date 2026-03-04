fbed6f3276a86308a608bfa451e5c7387b497e72
ayomialbertzon <ayomialbertzon@gmail.com>
2026-03-04 20:16:40 +0700

    update file terbaru praktikum 2

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+<div>
+    hubungi kami jika anda memiliki bantuan!
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<div>
+    Selamat Datang di Toko Sewa Baju Terbaik {{$nama}}<br>
+Anda adalah seorang {{$pekerjaan}}<br>
+alamat saya di {{$alamat}}<br>
+tanggal lahir saya yaitu {{$tanggal_lahir}}<br>
+Hobi saya yaitu {{$Hobi}}
+</div>
